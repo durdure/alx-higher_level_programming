3ef3baa68ba28201fdb10183f39249dfa11c6d67
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-08-09 15:40:18 +0300

    :sparkles: Cool :sparkles:

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+import random
+
+number = random.randint(-10000, 10000)
+
+# Get the last digit of the number using modulo division
+last_digit = number % 10
+
+# Print out the appropriate message based on the last digit of the number
+print("Last digit of", number, "is", last_digit, end=" ")
+
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
