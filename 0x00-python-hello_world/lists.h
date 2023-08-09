a1516515ea0022ca2091ec96ceef5e42fc34405f
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-08-09 11:11:08 +0300

    :sparkles: Cool :sparkles:

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,22 +3,17 @@
 
 #include <stdlib.h>
 
-/**
- * struct listint_s - singly linked list
- * @n: interger
- * @next: point to the next node
- *
- * Description: singly linked list node structure
- */
+
 typedef struct listint_s
 {
         int n;
         struct listint_s *next;
 } listint_t;
 
-int check_cycle(listint_t *list);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
+
