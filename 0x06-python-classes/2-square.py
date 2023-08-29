10b87c36790a8fcb96aaac2ba016a95877880fdb
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-08-29 19:27:05 +0300

    :sparkles: Cool :sparkles:

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new Square
+        """Initialize a new Square.
 
         Args:
             size (int): The size of the new square.
@@ -17,4 +17,3 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
