468cbd4f8b35fbf677067f64e7bda2a83f76da32
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-10-17 16:04:53 +0300

    :sparkles: Cool :sparkles:

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 --creates a table in given database and inserts
 -- values
 CREATE TABLE if NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, nane, score) VALUES (1, 'John', 10)
-INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3)
-INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14)
-INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8)
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8;)
