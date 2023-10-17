a0eac328b55d9afc378a6302605c0932c8cd8cd2
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-10-17 16:41:54 +0300

    :sparkles: Cool :sparkles:

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---creates a table in given database and inserts
+-- creates a table in given database and inserts
 -- values
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
