263164c422a38d9bde6f2c61c448b164ed492194
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-10-18 23:40:16 +0300

    :sparkles: Cool :sparkles:

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on server (in localhost).
-SHOW GRANT FOR `user_0d_1`@`localhost`;
-SHOW GRANT FOR `user_0d_2`@`localhost`;
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2.
+
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
