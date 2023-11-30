85518e58580c4727c90353ea39a8b62c5ac9443d
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-11-30 12:42:17 +0300

    python

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sX GET $1 -L
